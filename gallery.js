72eceb6145b23577523640aeb1702966f45d6c0a
joshuagdd <joshuagabrielledomingo@gmail.com>
2026-03-06 08:45:18 +0800

    Refactor upDate and unDo functions for image display

--- gallery.js
+++ gallery.js
@@ -1,23 +1,23 @@
 /*Name this external file gallery.js*/
 
 function upDate(previewPic){
- /* In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
-    
-    2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image 
-    */
-  
-	}
+    /* 1) Change the background image of the div with id="image"
+       to the source of the preview image */
 
-	function unDo(){
-     /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    */
-		
-	}
+    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
+
+    /* 2) Change the text of the div with id="image"
+       to the alt text of the preview image */
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+}
+
+function unDo(){
+    /* 1) Change the background image back to the original (empty) */
+
+    document.getElementById("image").style.backgroundImage = "url('')";
+
+    /* 2) Change the text back to the original text */
+
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+}
